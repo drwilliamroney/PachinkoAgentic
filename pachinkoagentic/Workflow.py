157bd3783a2297a305218a4b21e781b1be33dfb3
DrBill688 <drbill688@gmail.com>
2025-12-09 16:38:06 -0500

    Added flowcharting as algorithm

--- pachinkoagentic/Workflow.py
+++ pachinkoagentic/Workflow.py
@@ -19,6 +19,7 @@ import asyncio
 from .AIWrapper import AIWrapper
 from .Library import Library
 from .WorkflowEvent import WorkflowEventType, WorkflowEvent
+from .Flowchart import Flowchart, End, Call, Junction
 
 class Workflow:
     def __init__(self, agentic_code_generator: AIWrapper, llm: AIWrapper, library: Library, workflow_id: str):
@@ -32,37 +33,19 @@ class Workflow:
         You may not import any packages within the function either with import or from.  
           Assume 'import asyncio' has already been done outside of the function scope.
           Assume that every module in the library is defined within the parameter object.
-            For example, if the library spec includes Module: Umptyfratz, and function named foobar, then you should make function calls fully specified as {self.library.package}.Umptyfratz.foobar(...).
+            For example, if the library spec includes Module: Umptyfratz, and function named foobar, then you should make function calls fully specified as {self.library.package}.Umptyfratz.foobar(...).  
+            Built in functions should be called as {self.library.package}.foo(...).
+          All functions within the {self.library.package} object are async coroutines, not generators.  
+          When possible, you should run groups of coroutines and gather before proceeding.  Only await individual functions when there is no other option.
 
-        All functions and methods within the {self.library.package} object are async coroutines, not generators.  When possible, you should run groups of coroutines and gather before proceeding.  Only await individual functions when there is no other option.
-
-        If you cannot figure out a method of answering the user's question using the available library, then call: await {self.library.package}.Sample(<user's question as a quoted string>) which returns a string result.  
-            
-        Outputs should use await {self.library.package}.Output(<string to output>).  Do not use print().
-        
         Wrap the python function in tags so that the final output looks like this:
         [PYTHON BEGINS]
         async def {self._funcname}():
             ...
         [PYTHON ENDS]
-        
-        Second, you are to define a flowchart of the python function that you created.  This flowchart should be in SVG format and wrapped in tags so that the final output looks like this:
-        [FLOWCHART BEGINS]
-        <svg>...</svg>
-        [FLOWCHART ENDS]
-
-        This flowchart should be structured vertically starting at the top.  The flowchart symbology is as follows:
-          The start symbol, a solid black circle, should be in the top center.  This symbol should not have an id assigned.
-          Each async function call should be a square with a black outline, and id of "line#" using the line number within the python code (function declaration is line 1), and the line number as text, centered in the middle.
-          Each gather call should be a diamond with a black outline, an id of "line#" using the line number within the python code (function declaration is line 1),  and the line number displayed as text, centered in the middle.
-          At the end of the flow chart should be a hollow black circle with a double outline, and an id of "line1".
-          Each "row" of the flowchart should be centered.
-          No shapes should overlap.
-          The flowchart should have red lines connecting all shapes to indicate the process flow.  The connection points should start from the bottom center of the source shape and top center of the destination shape.
 
         Here is the library definition:
         '''
-
         return
     
     async def generate(self, question: str):
@@ -74,6 +57,7 @@ class Workflow:
         self.code = ''
         self.image = ''
         system_prompt = self._generator_prompt+self.library.swagger_docs()
+        await logger.debug(system_prompt)
         yield WorkflowEvent(event_type=WorkflowEventType.WORKFLOW_GENERATION_PROMPT, workflow_id=self.workflow_id, extra_data=system_prompt)
         llm_response = await self.agentic_code_generator.get_response(system_prompt=system_prompt,
                                                          question=question,
@@ -83,12 +67,12 @@ class Workflow:
         try:
             code = self.workplan.answer.split('[PYTHON BEGINS]')[1]
             if code is not None:
-                self.code = code.split('[PYTHON ENDS]')[0].lstrip()
+                self.code = code.split('[PYTHON ENDS]')[0].strip()
             await logger.debug(self.code)
-            image = self.workplan.answer.split('[FLOWCHART BEGINS]')[1]
-            if image is not None:
-                self.image = image.split('[FLOWCHART ENDS]')[0].lstrip()
             await logger.debug(self.code)
+            flowchart = Flowchart()
+            await flowchart.from_code(self.code)
+            self.image = await flowchart.svg()
             yield WorkflowEvent(event_type=WorkflowEventType.WORKFLOW_CODE, workflow_id=self.workflow_id, extra_data=self.code)
             yield WorkflowEvent(event_type=WorkflowEventType.WORKFLOW_IMAGE, workflow_id=self.workflow_id, extra_data=self.image)
         except Exception as e:
@@ -105,6 +89,7 @@ class Workflow:
         foo = asyncio.ensure_future(runner.exec_agentic_function(self._funcname, self.code))
         await logger.debug('Starting message pump')
         async for event in runner.event_stream:
+            await logger.debug(f'Got event: {event}')
             yield event
             if event.event_type == WorkflowEventType.WORKFLOW_END:
                 await logger.debug('Completion Event Detected, exiting loop')
@@ -114,3 +99,4 @@ class Workflow:
             await logger.debug('Final await on foo')
             await foo
         await logger.debug('Process complete')
+
